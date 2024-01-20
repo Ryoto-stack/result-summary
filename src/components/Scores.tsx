8b1adb89f7c86c4040f36a92c2f15b098d20481e
Kent Escala <escalakent18@gmail.com>
2024-01-20 19:37:39 +0800

    1st commit

---
+++ src/components/Scores.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Scores() {
+  return (
+    <div className='bg-[#FEF6F4] flex items-center justify-between max-w-72 px-auto h-full'>
+        <p className='w-72  '>
+            Reaction
+        </p>
+        <p>
+            80 / 100
+        </p>
+    </div>
+  )
+}
